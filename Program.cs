e498f2558949bf18f059cbfa5be1b1490beb39f7
vrushali joshi <vrushalijoshi589@mail.com>
2022-05-25 16:07:19 +0530

    push element in stack

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueProblem
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Do Stack Operation!");
+            StackQueue stacksProblem = new StackQueue();
+            stacksProblem.push(70);
+            stacksProblem.push(30);
+            stacksProblem.push(56);
+            stacksProblem.display();
+
+        }
+    }
+}
+
---
+++ StackQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueProblem
+{
+    public class StackQueue
+    {
+
+        public Node top;
+        public StackQueue()
+        {
+            this.top = null;
+
+        }
+        internal void push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top != null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("pushed into stack{0}", value);
+
+
+        }
+        internal void display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+
+        }
+    }
+}
